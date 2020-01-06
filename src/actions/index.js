467fbd2d16d33ecde0d0572ad8a582f2606961c7
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 13:34:37 -0500

    initialize addCar method that posts to API and returns to callback homepage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 // TODO: add and export your own actions
 export const FETCH_CARS = "FETCH_CARS";
+export const ADD_CAR = "ADD_CAR";
 
 export const fetchCars = (garage) => {
   const url = `https://wagon-garage-api.herokuapp.com/${garage}/cars`;
@@ -9,5 +10,20 @@ export const fetchCars = (garage) => {
   return {
     type: FETCH_CARS,
     payload: promise
+  };
+};
+
+export const addCar = (garage, car, callback) => {
+  const url = `https://wagon-garage-api.herokuapp.com/${garage}/cars`;
+  const request = fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(car)
+  }).then(response => response.json())
+    .then(callback);
+
+  return {
+    type: ADD_CAR,
+    payload: request
   };
 };
