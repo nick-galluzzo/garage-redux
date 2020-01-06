3b9361a22812efd60cb56a4d3b4a3efd8b6f6b20
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 18:06:27 -0500

    add fetch_car case

--- src/reducers/carsReducer.js
+++ src/reducers/carsReducer.js
@@ -1,13 +1,16 @@
 // import cars from '../../assets/data/cars';
-import { FETCH_CARS } from '../actions/index';
+import { FETCH_CARS, FETCH_CAR } from '../actions/index';
 
-const carsReducer = (state = [], action) => {
-  if (state === null) {
+function carsReducer (state = [], action) {
+  if (state === undefined) {
     return [];
   }
   switch (action.type) {
     case FETCH_CARS: {
       return action.payload;
+    }
+    case FETCH_CAR: {
+      return [action.payload];
     }
     default:
       return state;
