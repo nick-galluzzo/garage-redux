0ef565edd63b596b8f695e17c3e28476075abf95
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 11:37:55 -0500

    add fetchCars action returning all cars in garage - fetched from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // TODO: add and export your own actions
+export const FETCH_CARS = "FETCH_CARS";
+
+export const fetchCars = (garage) => {
+  const url = `https://wagon-garage-api.herokuapp.com/${garage}/cars`;
+  const promise = fetch(url)
+    .then(response => response.json());
+
+  return {
+    type: FETCH_CARS,
+    payload: promise
+  };
+};
