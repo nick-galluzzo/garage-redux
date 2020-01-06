9d2ac262054976c758a8e729930965376de93066
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 18:51:43 -0500

    code cleanup

--- src/reducers/garageReducer.js
+++ src/reducers/garageReducer.js
@@ -1,4 +1,4 @@
-export const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
 
 function garageReducer(state = garageName, action) {
   if (state === undefined) {
