22eff0ebb704ba8308148feef222cc7c3fc2df67
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 10:47:00 -0500

    added store and connected CarsIndex container

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
-import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
+import store from './components/store';
+import CarsIndex from './containers/carsIndex';
+
 import '../assets/stylesheets/application.scss';
 
-const reducers = combineReducers({
-  // key: reducer
-});
-
-const middlewares = applyMiddleware(reduxPromise, logger);
-
-// render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={store}>
     <Router history={history}>
       <Switch>
-        TODO
+        <CarsIndex />
       </Switch>
     </Router>
   </Provider>,
