c60088c80db28badc0f9b5a1408920878588c6fa
Nick Galluzzo <n.galluzzo@gmail.com>
2020-01-06 13:40:41 -0500

    add carsReducer and formReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import garageReducer from './garageReducer';
 import carsReducer from './carsReducer';
 
 const rootReducer = combineReducers({
   garage: garageReducer,
-  cars: carsReducer
+  cars: carsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
